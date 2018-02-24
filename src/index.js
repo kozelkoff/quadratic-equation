7d2390f4d9ddfce2295897a51a17e758b4db8838
Pavel Kozelkov <pavelalexandrovich841@gmail.com>
2018-02-24 16:05:31 +0300

    solution sh: wq: command not found

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+    var a, b, c;
+
+    var compareFunction = function compareNumeric(a, b) {
+        return a - b;
+    }
+
+
+    var finding = equation.split(' ');
+    a = finding[0];
+    if (finding[3]== '-') {
+        b = finding[4] * (-1);
+    }
+    else b = finding[4];
+    if (finding[7]== '-') {
+        c = finding[8] * (-1);
+    }
+    else c = finding[8];
+
+    var discriminant = b*b - 4 * a * c;
+    discriminant = (Math.sqrt(discriminant));
+
+    var x1 = (-b + discriminant)/(2*a);
+    var x2 = (-b - discriminant)/(2*a);
+
+
+    let solutions = [];
+    solutions[0]=Math.round(x1);
+    solutions[1]=Math.round(x2);
+
+    solutions.sort(compareFunction);
+
+    return solutions;
+
+    
 }
